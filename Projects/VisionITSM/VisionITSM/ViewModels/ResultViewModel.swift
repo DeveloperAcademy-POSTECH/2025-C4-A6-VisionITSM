180410cc407b38df60ead1f7d262769250d2286b
Yeon Seonae <ifindary@gmail.com>
2025-07-12 21:20:31 +0900

    feat: MVVM 패턴 적용해서 파일트리 구성

---
+++ Projects/VisionITSM/VisionITSM/ViewModels/ResultViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  ResultViewModel.swift
+//  VisionITSM
+//
+//  Created by 선애 on 7/12/25.
+//
+
---
+++ Projects/VisionITSM/VisionITSM/Views/ResultView/ResultWindow.swift
@@ -0,0 +1,7 @@
+//
+//  ResultWindow.swift
+//  VisionITSM
+//
+//  Created by 선애 on 7/12/25.
+//
+
