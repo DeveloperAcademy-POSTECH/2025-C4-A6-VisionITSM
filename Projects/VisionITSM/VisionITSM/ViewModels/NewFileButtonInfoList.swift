b4590513cbbddd0ab1b8885a605d7f9affd23abe
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-07-22 20:50:24 +0900

    [feat] #41 : PPT업로드 모달 디자인 변경

---
+++ Projects/VisionITSM/VisionITSM/ViewModels/NewFileButtonInfoList.swift
@@ -0,0 +1,8 @@
+//
+//  NewFileButtonInfoList.swift
+//  VisionITSM
+//
+//  Created by 진아현 on 7/22/25.
+//
+
+import Foundation
---
+++ Projects/VisionITSM/VisionITSM/Views/Home/NewFileModalView.swift
@@ -0,0 +1,18 @@
+//
+//  NewFileModalView.swift
+//  VisionITSM
+//
+//  Created by 진아현 on 7/22/25.
+//
+
+import SwiftUI
+
+struct NewFileModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewFileModalView()
+}
